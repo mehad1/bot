5a3b065631f29fb60e45cb3e136ba03d020931e0
mehad1 <phantom_mehad@yahoo.com>
2016-05-25 20:07:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 
 // index
 app.get('/', function (req, res) {
-	res.send('hello world i am a secret bot')
+	res.send('hello world i am a bot')
 })
 
 // for facebook verification
