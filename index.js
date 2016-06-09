a23380a4a0749c42719ce3e366cca01747ac4c5e
mehad1 <phantom_mehad@yahoo.com>
2016-06-09 20:14:43 +0600

    Update index.js

--- index.js
+++ index.js
@@ -38,8 +38,10 @@ app.post('/webhook/', function (req, res) {
 			}
 			
 			sendTextMessage(sender, ("Hi! I am offerbot here to guide you for the best dining experience"))
-			sendTextMessage(sender, ("Hi! 
-			I am offerbot here to guide you for the best dining experience"))
+			sendTextMessage(sender, ("To learn the best deals around ask me regarding the following"+
+			"1. Best iftar in Dhaka"+
+			"2. Best discounts in Dhaka"+
+			"3. Buy one get one free"))
 		}
 		if (event.postback) {
 			text = JSON.stringify(event.postback)
