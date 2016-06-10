f971387a392a214e0d114a1981f6a3d02e0257f5
mehad1 <phantom_mehad@yahoo.com>
2016-06-10 18:32:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -163,13 +163,13 @@ function sendGenericMessage(sender) {
           "elements": [{
             "title": "rift",
             "subtitle": "Next-generation virtual reality",
-            "item_url": "https://www.oculus.com/en-us/rift/",
-            "image_url": "attachment": {
-				 "type": "image",
-				 "payload": {
-        			 "url": "http://i.imgur.com/zYIlgBl.png"
+            "item_url": "attachment": {
+        			"type": "image",
+        			"payload": {
+          			"url": "http://i.imgur.com/zYIlgBl.png"
         }
       },
+            "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
             "buttons": [{
               "type": "web_url",
               "url": "https://www.oculus.com/en-us/rift/",
